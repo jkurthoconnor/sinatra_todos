1493307718037d48140e67d86aba6a6d0ed825e2
jkurthoconnor <jkurthoconnor@gmail.com>
2017-03-21 12:46:49 -0600

    add query logging

--- database_persistence.rb
+++ database_persistence.rb
@@ -2,17 +2,27 @@
 require 'pg'
 
 class DatabasePersistence
-  def initialize
+  def initialize(logger)
     @db = PG.connect(dbname: 'todos')
+    @logger = logger
+  end
+  
+  def query(statement, *params)
+    @logger.info "#{statement} : #{params}"
+    @db.exec_params(statement, params)
   end
 
   def find_list(id)
-    # @session[:lists].find { |list| list[:id] == id }
-  end
+    sql = 'SELECT * FROM lists WHERE id = $1;'
+    result = query(sql, id)
+
+    tuple = result.first
+    {id: tuple['id'], name: tuple['name'], todos:[] }
+   end
 
   def all_lists
     sql = 'SELECT * FROM lists;'
-    result = @db.exec(sql)
+    result = query(sql)
 
     result.map do |tuple|
       {id: tuple['id'], name: tuple['name'], todos:[] }
