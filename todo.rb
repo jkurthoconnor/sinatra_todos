da3690c556ef625232ac949b520de7d22a3538c8
jkurthoconnor <jkurthoconnor@gmail.com>
2017-03-21 16:04:35 -0600

    stylistic changes

--- todo.rb
+++ todo.rb
@@ -3,27 +3,26 @@ require 'sinatra/reloader' if development?
 require 'sinatra/content_for'
 require 'tilt/erubis'
 
-require_relative 'database_persistence' 
+require_relative 'database_persistence'
 
 configure do
   enable :sessions
   set :session_secret, 'secret'
-
-  set :erb, :escape_html => true
+  set :erb, escape_html: true
+  also_reload 'database_persistence.rb'
 end
-
 
 helpers do
   def completed?(list)
-    (total_todos(list) > 0) && (todos_remaining(list) == 0)
+    (total_todos(list) > 0) && todos_remaining(list).zero?
   end
 
   def list_class(list)
-    completed?(list) ? "complete" : ""
+    completed?(list) ? 'complete' : ''
   end
 
   def item_class(item)
-    item[:completed] == true ? "complete" : ""
+    item[:completed] == true ? 'complete' : ''
   end
 
   def todos_remaining(list)
@@ -49,30 +48,29 @@ helpers do
   end
 end
 
-
 def validate_and_load_list(id)
   list = @storage.find_list(id)
   return list if list
 
   session[:error] = "Requested list with id #{id} was not found."
-  redirect "/lists"
+  redirect '/lists'
 end
 
 # return an error message if name is invalid; return nil if name is valid
 def error_for_list_name(name)
   if !(1..100).cover?(name.size)
-    "List name must be between 1 and 100 characters."
+    'List name must be between 1 and 100 characters.'
   elsif @storage.all_lists.any? { |list| list[:name] == name }
-    "List names must be unique."
+    'List names must be unique.'
   end
 end
 
-#validate new todo item
+# validate new todo item
 def error_for_todo(name)
   if !(1..100).cover?(name.size)
-    "Todo item must be between 1 and 100 characters."
+    'Todo item must be between 1 and 100 characters.'
   elsif @todo_items.any? { |item| item[:name] == name }
-    "Todo items must be unique."
+    'Todo items must be unique.'
   end
 end
 
@@ -80,27 +78,23 @@ before do
   @storage = DatabasePersistence.new(logger)
 end
 
-
-get "/" do
-  redirect "/lists"
+get '/' do
+  redirect '/lists'
 end
 
-
 # view list of lists
-get "/lists" do
+get '/lists' do
   @lists = @storage.all_lists
   erb :lists, layout: :layout
 end
 
-
 # render new list form;
-get "/lists/new" do
+get '/lists/new' do
   erb :new_list, layout: :layout
 end
 
-
 # show single list;
-get "/lists/:id" do
+get '/lists/:id' do
   @list_id = params[:id].to_i
   @list = validate_and_load_list(@list_id)
   @todo_items = @list[:todos]
@@ -108,9 +102,8 @@ get "/lists/:id" do
   erb :single_list, layout: :layout
 end
 
-
 # create a new list
-post "/lists" do
+post '/lists' do
   list_name = params[:list_name].strip
   error = error_for_list_name(list_name)
 
@@ -119,21 +112,19 @@ post "/lists" do
     erb :new_list, layout: :layout
   else
     @storage.create_list(list_name)
-    session[:success] = "The list has been created!"
-    redirect "/lists"
+    session[:success] = 'The list has been created!'
+    redirect '/lists'
   end
 end
 
-
-#render edit list form
-get "/lists/:id/edit" do
+# render edit list form
+get '/lists/:id/edit' do
   @list = validate_and_load_list(params[:id].to_i)
   erb :edit_list, layout: :layout
 end
 
-
-#update list name
-post "/lists/:id" do
+# update list name
+post '/lists/:id' do
   new_name = params[:new_name].strip
   @list = validate_and_load_list(params[:id].to_i)
   error = error_for_list_name(new_name)
@@ -143,29 +134,27 @@ post "/lists/:id" do
     erb :edit_list, layout: :layout
   else
     @storage.update_list_name(params[:id].to_i, new_name)
-    session[:success] = "The list has been updated!"
-    redirect "/lists"
+    session[:success] = 'The list has been updated!'
+    redirect '/lists'
   end
 end
 
-
 # delete a list
-post "/lists/:id/delete" do
+post '/lists/:id/delete' do
   list = validate_and_load_list(params[:id].to_i)
 
   @storage.delete_list(params[:id].to_i)
 
-  if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
-    "/lists"
+  if env['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'
+    '/lists'
   else
     session[:success] = "'#{list[:name]}' has been deleted!"
-    redirect "/lists"
+    redirect '/lists'
   end
 end
 
-
 # add a new todo item to a list
-post "/lists/:id/todos" do
+post '/lists/:id/todos' do
   @list_id = params[:id].to_i
   @list = validate_and_load_list(@list_id)
   @todo_items = @list[:todos]
@@ -177,52 +166,48 @@ post "/lists/:id/todos" do
     session[:error] = error
     erb :single_list, layout: :layout
   else
-    id = next_todo_id(@todo_items)
     @storage.add_todo(@list_id, todo)
-    session[:success] = "The todo has been added!"
+    session[:success] = 'The todo has been added!'
     redirect "/lists/#{@list_id}"
   end
 end
-
 
 # delete a todo from a list
-post "/lists/:id/todos/:item_id/delete" do
+post '/lists/:id/todos/:item_id/delete' do
   @list_id = params[:id].to_i
   @list = validate_and_load_list(@list_id)
   id = params[:item_id].to_i
   @todo_items = @list[:todos]
 
-  @storage.delete_todo(@list_id, id)
+  @storage.delete_todo(id)
 
-  if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
+  if env['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'
     status 204 # ok no content
   else
-    session[:success] = "Item deleted!"
+    session[:success] = 'Item deleted!'
     redirect "/lists/#{@list_id}"
   end
 end
 
-
 # update a todo's status
-post "/lists/:id/todos/:item_id" do
+post '/lists/:id/todos/:item_id' do
   @list_id = params[:id].to_i
   @list = validate_and_load_list(@list_id)
   id = params[:item_id].to_i
   @todo_items = @list[:todos]
 
-  @storage.update_todo(@list_id, id)
-  session[:success] = "Item updated!"
+  @storage.update_todo(id)
+  session[:success] = 'Item updated!'
   redirect "/lists/#{@list_id}"
 end
 
-
 # completes all items on list
-post "/lists/:id/complete" do
+post '/lists/:id/complete' do
   @list_id = params[:id].to_i
   @list = validate_and_load_list(@list_id)
   @todo_items = @list[:todos]
 
   @storage.complete_all(@list_id)
-  session[:success] = "List completed!"
+  session[:success] = 'List completed!'
   redirect "/lists/#{@list_id}"
 end
